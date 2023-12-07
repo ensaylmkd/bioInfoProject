b9a64a3af25fa702c30e511b6f4968d948dbbbd2
yassine <yassine.lamkaddem34@gmail.com>
2023-12-07 13:50:36 +0100

    I finit + erreur detecter

--- bioInfo.py
+++ bioInfo.py
@@ -1,3 +1,7 @@
+'''Erreur à corriger : les résultats des requetes changent avec le temps 
+(renvoie les résultats dans un ordre different) et certaine presentation 
+de donné (étape E) ne s'adapte pas à ces changements donc sont faux '''
+
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
@@ -5,25 +9,27 @@ from Bio import Entrez
 from Bio.Align.Applications import MafftCommandline
 
 Entrez.email = "user@gmail.com"
-# ////////// A //////////
+# /////////////////////// A ////////////////////////
 
 # setup
-RECHERCHE = "nucleotide",'(SARS-CoV-2[organism] AND refseq []) OR SARSr-CoV RaTG13 genome OR MP789 MT121216'
-NEW_FILE = "seq_covid"
+RECHERCHE_A = "nucleotide",'(SARS-CoV-2[organism] AND refseq []) OR SARSr-CoV RaTG13 genome OR MP789 MT121216'
+NEW_FILE_A = "seq_covid"
 
 def researchToGbSeq(search,newFileName):
-    '''Ecris le fichier gb correspondant à une recherche dans nucleotide '''
-    # on recupere les id des séquences que l'on veut
+    '''Ecris le fichier gb correspondant à une recherche dans nucleotide 
+        search      : liste str : contient le db et la recherche 
+        newFileName : str       : le nom du fichier qui sera creer'''
+    # on recupere les id des séquences que l'on veut avec une requete
     request = Entrez.esearch(search[0],search[1])
     research = Entrez.read(request)
     request.close()
-    # on récupere le fetch des sequences en gb
-    request = Entrez.efetch(db='nucleotide',id=research['IdList'],rettype='gb',retmode='text')
-    # on les converties en fichier gb 
+    # on récupere le fetch des sequences en gb avec une requete
+    request = Entrez.efetch(db=search[0],id=research['IdList'],rettype='gb',retmode='text')
+    # on les converties en fichier gb à l'aide de SeqIO
     seqs = SeqIO.parse(request,'gb')
     SeqIO.write(seqs,f"{newFileName}.gb","gb")
     request.close()
-#researchToGbSeq(RECHERCHE,NEW_FILE)
+# researchToGbSeq(RECHERCHE_A,NEW_FILE_A)
 
 # ////////// B //////////
 
@@ -40,13 +46,13 @@ def gbToInfo(file):
             cptGene=0
             infoGenes=[]
             for feature in seq.features:
-                if feature.type == 'gene': # compte le nb de gene
+                if feature.type == 'gene':                                 # compte le nb de gene
                     cptGene+=1
-            if feature.type =='CDS':
-                infoGenes.append([feature.qualifiers['gene'][0],      # infoGenes contient des listes avec :
-                                feature.location,                     # nom du gene, sa position et l'id de la proteine codée
-                                feature.qualifiers['protein_id'][0]]) # de chaque CDS 
-
+                if feature.type =='CDS':
+                    infoGenes.append([feature.qualifiers['gene'][0],       # infoGenes contient des listes avec :
+                                    feature.location,                      # nom du gene, sa position et l'id de la proteine codée
+                                    feature.qualifiers['protein_id'][0]])  # de chaque CDS  
+                                
             fd.write("\n///////////////////////////////////////////////////////\n\n")
             fd.write(f"Organism name: {seq.annotations['organism']}\n")                 # nom de l'organism
             fd.write(f"Taxonomy: {seq.annotations['taxonomy']}\n")                      # taxonomie
@@ -57,15 +63,15 @@ def gbToInfo(file):
 
             fd.write(f"Genes :\n")                                                      # info sur les genes
             for infoGene in infoGenes:
-                fd.write(f" - name: {str(infoGene[0]): <12}")
-                fd.write(f"location: {str(infoGene[1]): <45}")
-                fd.write(f"protein_ID: {infoGene[2]}")
+                fd.write(" "*3+f"- name: {infoGene[0]}")       # Remi vient recentrer ici jsp faire (regarde le .txt)
+                fd.write(" "*6 + f"location: {infoGene[1]}")
+                fd.write(" "*6 + f"protein_ID: {infoGene[2]}")
                 fd.write("\n")
 
-gbToInfo(FILE)
+# gbToInfo(FILE)
 # //////////////////// C ///////////////////////
 
-def multiFastaS(gene: str="S"):
+def multiFasta(gene: str="S"):
     seqs = list(SeqIO.parse('seq_covid.gb','gb'))
     with open("spike.fasta",'w') as fd:                                           # On ecrit dans le fichier spike.fasta :
         for seq in seqs:                                                          #     (pour chaque sequence dans seq_covid.gb
@@ -105,7 +111,7 @@ def comparaison(gene: str="S"):
     for i in resultat:  
         f.write(f"{i[0]:^15}{i[1]:^15}{i[2]:^15}{i[3]:^15}\n")
 
- # /////////////////// Fin du F /////////////////////////
+ # //// Fin du F /////
 
     taille = len(seqs[0].seq)
     print(f"Il y a : {(taille-difference[0])/taille*100:.2f}% de lettres identiques en l'Homme et la Chauve-souris pour le gene {gene}.")
@@ -119,15 +125,15 @@ def C_F(gene: str|None=None):
         gene = input("Quel gene choisissez vous : (S/M/N)")
     if gene not in ["S", "M", "N"]:
         gene = "S"
-    multiFastaS(gene)
+    multiFasta(gene)
     alignement()
     comparaison(gene)
 
 # /////////////////// H /////////////////////////
 
-C_F("S")
-C_F("M")
-C_F("N")
+# C_F("S")
+# C_F("M")
+# C_F("N")
 """
 Il y a : 90.04% de lettres identiques en l'Homme et la Chauve-souris pour le gene S.
 Il y a : 97.41% de lettres identiques en l'Homme et le Pangolin pour le gene S.
@@ -140,6 +146,24 @@ Voici ce qu'on obtient en comparant les genes S, M et N, elles se ressemblent au
 ce qui est trop élevé pour être le fruit du hasard.
 Et les genes du pangolin sont plus proche de ceux de l'Homme que la Chauve-souris.
 """
+# ////////////////// I ///////////////////////
+def covidGeneToGb(gene):
+    # on récupere les id des proteines avec la meme methode que à l'etape C 
+    tabProtein=[]
+    seqs = list(SeqIO.parse('seq_covid.gb','gb'))                                      
+    for seq in seqs:                                                          
+        for feature in seq.features:                                          
+            if feature.type == 'CDS' and 'translation' in feature.qualifiers: 
+                if feature.qualifiers['gene'][0] == gene :
+                    tabProtein.append(feature.qualifiers['protein_id'][0])
+    rechercheI=' OR '.join(tabProtein)
+    # avec la fonction de l'étape A
+    researchToGbSeq(['protein',rechercheI],f'covidProt_{gene}')
+
+covidGeneToGb('S')
+
+               
+
 
 
 
